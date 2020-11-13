7c51dcd38ad4e908c3e35c4d4009838702e04dd8
mgroff38 <71781107+mgroff38@users.noreply.github.com>
2020-11-13 12:23:40 -0500

    add *for host

--- bbq/settings.py
+++ bbq/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'iifz=*$x_2(*#-8g539_*-=o(#5+r_s+y7y_d_ioh3x!p$9=+2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.bbq2go.xyz', 'bbq2go.xyz', '127.0.0.1', 'localhost', '161.35.60.39']
+ALLOWED_HOSTS = ['*']
+
+# 'www.bbq2go.xyz', 'bbq2go.xyz', '127.0.0.1', 'localhost', '161.35.60.39'
 
 SITE_ID = 1
 
