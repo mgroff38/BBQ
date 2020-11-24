8ff13f956643af0edac99eb9518132a197269c2e
mgroff38 <71781107+mgroff38@users.noreply.github.com>
2020-11-24 13:58:25 -0500

    turn off debug

--- bbq/settings.py
+++ bbq/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.bbq2go.xyz', 'bbq2go.xyz', '127.0.0.1', 'localhost', '161.35.60.39']
 
